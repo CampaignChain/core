e073fce6d6f30e4f9691e6ecfe98f78d073a9961
Sandro Groganz <sandro@campaignchain.com>
2014-11-06 23:07:04 +0100

    CE-104 Make Dashboard page the About page

---
+++ Controller/AboutController.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the CampaignChain package.
+ *
+ * (c) Sandro Groganz <sandro@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CampaignChain\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AboutController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render(
+            'CampaignChainCoreBundle:About:index.html.twig',
+            array(
+                'page_title' => 'About CampaignChain'
+            )
+        );
+    }
+}
