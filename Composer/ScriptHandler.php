f07a39a2d5b1ad70f96fb2513a5046c4416a4c38
Sandro Groganz <sandro@campaignchain.com>
2015-09-07 21:50:47 +0200

    CE-353 bootstrap-clockpicker.min.js missing upon installation at step 1

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -50,7 +50,7 @@ class ScriptHandler extends SensioScriptHandler
             return;
         }
 
-        self::executeCommand($event, $consoleDir, 'doctrine:schema:update --force', $options['process-timeout']);
-        self::executeCommand($event, $consoleDir, 'campaignchain:module:update', $options['process-timeout']);
+        //self::executeCommand($event, $consoleDir, 'doctrine:schema:update --force', $options['process-timeout']);
+        self::executeCommand($event, $consoleDir, 'campaignchain:module:update --config-only', $options['process-timeout']);
     }
 }
