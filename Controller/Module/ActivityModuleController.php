e266670729cb4bfcabd91afe7591e016e3db2dd5
Sandro Groganz <sandro@campaignchain.com>
2015-09-28 15:03:44 +0200

    CE-381 Refactor module framework

--- Controller/Module/ActivityModuleController.php
+++ Controller/Module/ActivityModuleController.php
@@ -10,6 +10,7 @@
 
 namespace CampaignChain\CoreBundle\Controller\Module;
 
+use CampaignChain\CoreBundle\Entity\Campaign;
 use CampaignChain\CoreBundle\Entity\Location;
 use CampaignChain\CoreBundle\Entity\Medium;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -22,8 +23,20 @@ class ActivityModuleController extends Controller
 {
     protected $parameters;
 
+    protected $handler = null;
+
+    protected $campaign;
+
+    protected $activity;
+
+    protected $operations = array();
+
     public function setParameters($parameters){
         $this->parameters = $parameters;
+
+        if(isset($this->parameters['handler'])){
+            $this->handler = $this->get($this->parameters['handler']);
+        }
     }
 
     public function newAction(Request $request)
@@ -32,50 +45,21 @@ class ActivityModuleController extends Controller
          * Get context from user's choice.
          */
         $wizard = $this->get('campaignchain.core.activity.wizard');
-        $campaign = $wizard->getCampaign();
-        $activity = $wizard->getActivity();
-        $activity->setEqualsOperation($this->parameters['equals_operation']);
+        $campaignService = $this->get('campaignchain.core.campaign');
+        $this->campaign = $campaignService->getCampaign($wizard->getCampaign());
+        $this->activity = $wizard->getActivity();
+        $this->activity->setEqualsOperation($this->parameters['equals_operation']);
         $locationService = $this->get('campaignchain.core.location');
-        $location = $locationService->getLocation($wizard->getLocation());
+        $this->location = $locationService->getLocation($wizard->getLocation());
 
-        /*
-         * Configure the Activity's form type.
-         */
-        $activityFormType = $this->get('campaignchain.core.form.type.activity');
-        $activityFormType->setBundleName($this->parameters['activity_bundle_name']);
-        $activityFormType->setModuleIdentifier(
-            $this->parameters['activity_module_identifier']
+        $form = $this->createForm(
+            $this->getActivityFormType(), $this->activity
         );
-        $activityFormType->setCampaign($campaign);
-
-        /*
-         * Set the Operation forms for this Activity.
-         */
-        if(!is_array($this->parameters['operation_forms'])){
-            throw new \Exception('No operation forms defined');
-        } else {
-            foreach($this->parameters['operation_forms'] as $operationForm){
-                $operationFormType = new $operationForm(
-                    $this->getDoctrine()->getManager(),
-                    $this->get('service_container')
-                );
-
-                $operationFormType->setLocation($location);
-
-                $operationForms[] = array(
-                    'identifier' => $this->parameters['operation_module_identifier'],
-                    'form' => $operationFormType
-                );
-            }
-        }
-        $activityFormType->setOperationForms($operationForms);
-
-        $form = $this->createForm($activityFormType, $activity);
 
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $activity = $wizard->end();
+            $this->activity = $wizard->end();
 
             // Get the operation module.
             $operationService = $this->get('campaignchain.core.operation');
@@ -87,9 +71,9 @@ class ActivityModuleController extends Controller
             if($this->parameters['equals_operation']) {
                 // The activity equals the operation. Thus, we create a new operation with the same data.
                 $operation = new Operation();
-                $operation->setName($activity->getName());
-                $operation->setActivity($activity);
-                $activity->addOperation($operation);
+                $operation->setName($this->activity->getName());
+                $operation->setActivity($this->activity);
+                $this->activity->addOperation($operation);
                 $operationModule->addOperation($operation);
                 $operation->setOperationModule($operationModule);
 
@@ -102,20 +86,24 @@ class ActivityModuleController extends Controller
                     $this->parameters['location_module_identifier']
                 );
 
-                $statusLocation = new Location();
-                $statusLocation->setLocationModule($locationModule);
-                $statusLocation->setParent($activity->getLocation());
-                $statusLocation->setName($activity->getName());
-                $statusLocation->setStatus(Medium::STATUS_UNPUBLISHED);
-                $statusLocation->setOperation($operation);
-                $operation->addLocation($statusLocation);
+                $operationLocation = new Location();
+                $operationLocation->setLocationModule($locationModule);
+                $operationLocation->setParent($this->activity->getLocation());
+                $operationLocation->setName($this->activity->getName());
+                $operationLocation->setStatus(Medium::STATUS_UNPUBLISHED);
+                $operationLocation->setOperation($operation);
+                $operation->addLocation($operationLocation);
 
                 // Get the status data from request.
-                $status = $form->get($this->parameters['operation_module_identifier'])->getData();
+                $operationDetails =
+                    $form->get($this->parameters['operation_module_identifier'])
+                        ->getData();
                 // Link the status with the operation.
-                $status->setOperation($operation);
+                $operationDetails->setOperation($operation);
             } else {
-                throw new \Exception('Multiple Operations for one Activity not implemented yet.');
+                throw new \Exception(
+                    'Multiple Operations for one Activity not implemented yet.'
+                );
             }
 
             $repository = $this->getDoctrine()->getManager();
@@ -124,14 +112,21 @@ class ActivityModuleController extends Controller
             try {
                 $repository->getConnection()->beginTransaction();
 
-                $repository->persist($activity);
-                $repository->persist($status);
+                $repository->persist($this->activity);
+                $repository->persist($operationDetails);
 
-                // We need the activity ID for storing the hooks. Hence we must flush here.
+                // We need the activity ID for storing the hooks. Hence we must
+                // flush here.
                 $repository->flush();
 
                 $hookService = $this->get('campaignchain.core.hook');
-                $activity = $hookService->processHooks($this->parameters['activity_bundle_name'], $this->parameters['activity_module_identifier'], $activity, $form, true);
+                $this->activity = $hookService->processHooks(
+                    $this->parameters['activity_bundle_name'],
+                    $this->parameters['activity_module_identifier'],
+                    $this->activity,
+                    $form,
+                    true
+                );
                 $repository->flush();
 
                 $repository->getConnection()->commit();
@@ -142,7 +137,7 @@ class ActivityModuleController extends Controller
 
             $this->get('session')->getFlashBag()->add(
                 'success',
-                'Your new activity <a href="'.$this->generateUrl('campaignchain_core_activity_edit', array('id' => $activity->getId())).'">'.$activity->getName().'</a> was created successfully.'
+                'Your new activity <a href="'.$this->generateUrl('campaignchain_core_activity_edit', array('id' => $this->activity->getId())).'">'.$this->activity->getName().'</a> was created successfully.'
             );
 
             // Status Update to be sent immediately?
@@ -156,23 +151,175 @@ class ActivityModuleController extends Controller
             return $this->redirect($this->generateUrl('campaignchain_core_activities'));
         }
 
-        $campaignService = $this->get('campaignchain.core.campaign');
-        $campaign = $campaignService->getCampaign($campaign);
-
         return $this->render(
             'CampaignChainCoreBundle:Operation:new.html.twig',
             array(
                 'page_title' => 'New Activity',
-                'activity' => $activity,
-                'campaign' => $campaign,
-                'campaign_module' => $campaign->getCampaignModule(),
+                'activity' => $this->activity,
+                'campaign' => $this->campaign,
+                'campaign_module' => $this->campaign->getCampaignModule(),
                 'channel_module' => $wizard->getChannelModule(),
                 'channel_module_bundle' => $wizard->getChannelModuleBundle(),
-                'location' => $wizard->getLocation(),
+                'location' => $this->location,
                 'form' => $form->createView(),
                 'form_submit_label' => 'Save',
                 'form_cancel_route' => 'campaignchain_core_activities_new'
             ));
 
+    }
+
+    public function editAction(Request $request, $id)
+    {
+        $activityService = $this->get('campaignchain.core.activity');
+        $this->activity = $activityService->getActivity($id);
+        $this->campaign = $this->activity->getCampaign();
+        $this->location = $this->activity->getLocation();
+
+        if($this->parameters['equals_operation']) {
+            // Get the one operation.
+            $this->operations[] = $activityService->getOperation($id);
+        } else {
+            throw new \Exception(
+                'Multiple Operations for one Activity not implemented yet.'
+            );
+        }
+
+        $form = $this->createForm(
+            $this->getActivityFormType(), $this->activity
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // Get the status data from request.
+            $operationDetails =
+                $form->get($this->parameters['operation_module_identifier'])
+                    ->getData();
+
+            $repository = $this->getDoctrine()->getManager();
+
+            // Make sure that data stays intact by using transactions.
+            try {
+                $repository->getConnection()->beginTransaction();
+
+                if($this->parameters['equals_operation']) {
+                    // The activity equals the operation. Thus, we update the operation with the same data.
+                    $this->operations[0]->setName($this->activity->getName());
+                    $repository->persist($this->operations[0]);
+                } else {
+                    throw new \Exception(
+                        'Multiple Operations for one Activity not implemented yet.'
+                    );
+                }
+
+                $repository->persist($operationDetails);
+
+                $hookService = $this->get('campaignchain.core.hook');
+                $this->activity = $hookService->processHooks(
+                    $this->parameters['activity_bundle_name'],
+                    $this->parameters['activity_module_identifier'],
+                    $this->activity,
+                    $form
+                );
+                $repository->persist($this->activity);
+
+                $repository->flush();
+
+    //            // Status Update should be sent immediately
+    //            if ($form->get('actions')->get('send')->isClicked()) {
+    //                $job = $this->get('campaignchain.job.operation.twitter.update_status');
+    //                $job->execute($operation);
+    //
+    //                // TODO: If this previously was a scheduled activity, then reset the schedule
+    //            }
+                $repository->getConnection()->commit();
+            } catch (\Exception $e) {
+                $repository->getConnection()->rollback();
+                throw $e;
+            }
+
+            $this->get('session')->getFlashBag()->add(
+                'success',
+                'Your activity <a href="'.$this->generateUrl('campaignchain_core_activity_edit', array('id' => $this->activity->getId())).'">'.$this->activity->getName().'</a> was edited successfully.'
+            );
+
+            if ($form->get('campaignchain_hook_campaignchain_due')->has('execution_choice') && $form->get('campaignchain_hook_campaignchain_due')->get('execution_choice')->getData() == 'now') {
+                $job = $this->get($this->parameters['operation_job']);
+                $job->execute($this->operations[0]->getId());
+                // TODO: Add different flashbag which includes link to posted message on Facebook
+            }
+
+            return $this->redirect($this->generateUrl('campaignchain_core_activities'));
+        }
+
+        return $this->render(
+            'CampaignChainCoreBundle:Operation:new.html.twig',
+            array(
+                'page_title' => 'Edit Activity',
+                'activity' => $this->activity,
+                'form' => $form->createView(),
+                'form_submit_label' => 'Save',
+                'form_cancel_route' => 'campaignchain_core_activities'
+            ));
+    }
+
+    /**
+     * Configure an Activity's form type.
+     *
+     * @return object
+     */
+    private function getActivityFormType()
+    {
+        $activityFormType = $this->get('campaignchain.core.form.type.activity');
+        $activityFormType->setBundleName($this->parameters['activity_bundle_name']);
+        $activityFormType->setModuleIdentifier(
+            $this->parameters['activity_module_identifier']
+        );
+        $activityFormType->setOperationForms(
+            $this->getOperationFormTypes()
+        );
+        $activityFormType->setCampaign($this->campaign);
+
+        return $activityFormType;
+    }
+
+    /**
+     * Set the Operation forms for this Activity.
+     *
+     * @return array
+     * @throws \Exception
+     */
+    private function getOperationFormTypes()
+    {
+        if(!is_array($this->parameters['operation_forms'])){
+            throw new \Exception('No operation forms defined');
+        } else {
+            if($this->parameters['equals_operation']) {
+                $operationForm = $this->parameters['operation_forms'][0];
+                $operationFormType = new $operationForm(
+                    $this->getDoctrine()->getManager(),
+                    $this->get('service_container')
+                );
+
+                $operationFormType->setLocation($this->location);
+
+                if($this->handler && isset($this->operations[0])){
+                    $operationFormType->setOperationDetail(
+                        $this->handler->getOperationDetail($this->operations[0])
+                    );
+                }
+
+                $operationForms[] = array(
+                    'identifier' => $this->parameters['operation_module_identifier'],
+                    'form' => $operationFormType
+                );
+            } else {
+                throw new \Exception(
+                    'Multiple Operations for one Activity not implemented yet.'
+                );
+            }
+        }
+
+        return $operationForms;
     }
 }
