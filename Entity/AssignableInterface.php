9238006dc47780196df480c23eb280c477096d34
Sandro Groganz <sandro@campaignchain.com>
2015-11-04 10:48:48 +0100

    CE-440 Attempted to load class "MilestoneRepository" when viewing calendar

--- Entity/AssignableInterface.php
+++ Entity/AssignableInterface.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andrasratz
- * Date: 09/10/15
- * Time: 17:07
+/*
+ * This file is part of the CampaignChain package.
+ *
+ * (c) CampaignChain Inc. <info@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace CampaignChain\CoreBundle\Entity;
