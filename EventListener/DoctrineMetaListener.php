5bfa5117f3aaeb1f4a72879c0ce09bbb1edd63a4
tico86 <timo.collmann@ymc.ch>
2016-01-20 12:22:15 +0100

    #CE-188 Modified date not being saved for Activity
    Added for each loop to check for entity updates.

--- EventListener/DoctrineMetaListener.php
+++ EventListener/DoctrineMetaListener.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * This file is part of the CampaignChain package.
- *
- * (c) CampaignChain, Inc. <info@campaignchain.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of the CampaignChain package.
+*
+* (c) CampaignChain, Inc. <info@campaignchain.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 namespace CampaignChain\CoreBundle\EventListener;
 
@@ -21,24 +21,31 @@ class DoctrineMetaListener
 
         foreach ($uow->getScheduledEntityInsertions() as $entity) {
             /*
-             * Check if the class is a child of the Meta class, the latter
-             * handling the automatic createDate and modifiedDate methods.
-             */
+            * Check if the class is a child of the Meta class, the latter
+            * handling the automatic createDate and modifiedDate methods.
+            */
             if (!is_subclass_of($entity, 'CampaignChain\CoreBundle\Entity\Meta')) {
                 continue;
             }
-
             $logMetadata = $em->getClassMetadata(get_class($entity));
 
-            if ($entity->getCreatedDate() == null) {
-                $entity->setCreatedDate(new \DateTime('now', new \DateTimeZone('UTC')));
-            } else {
-                $entity->setModifiedDate(new \DateTime('now', new \DateTimeZone('UTC')));
-            }
+
+            $entity->setCreatedDate(new \DateTime('now', new \DateTimeZone('UTC')));
+            $entity->setModifiedDate($entity->getCreatedDate());
+
 
             $em->persist($entity);
             $classMetadata = $em->getClassMetadata(get_class($entity));
             $uow->recomputeSingleEntityChangeSet($classMetadata, $entity);
+
         }
+        foreach ($uow->getScheduledEntityUpdates() as $entity) {
+            if (!is_subclass_of($entity, 'CampaignChain\CoreBundle\Entity\Meta')) {
+                continue;
+            }
+            $logMetadata = $em->getClassMetadata(get_class($entity));
+            $entity->setModifiedDate(new \DateTime('now', new \DateTimeZone('UTC')));
+        }
+
     }
 }
