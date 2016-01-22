e9ee8d08b2df1a2f32aa9e6427ec0c1094510bdd
rogamoore <roger.oegretir@ymc.ch>
2016-01-22 15:04:02 +0100

    add new exception type

---
+++ Exception/ExternalApiException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * This file is part of the CampaignChain package.
+ *
+ *  (c) CampaignChain, Inc. <info@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ *
+ */
+
+namespace CampaignChain\CoreBundle\Exception;
+
+class ExternalApiException extends \RuntimeException
+{
+}
