d29aa95f8aeb3aa1c12ba01f57ab48da42322c13
rogamoore <roger.oegretir@ymc.ch>
2016-01-22 15:06:13 +0100

    write external exceptions to separate monolog channel

---
+++ EventListener/ExternalExceptionListener.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *
+ * This file is part of the CampaignChain package.
+ *
+ *  (c) CampaignChain, Inc. <info@campaignchain.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ *
+ */
+
+namespace CampaignChain\CoreBundle\EventListener;
+
+use CampaignChain\CoreBundle\Exception\ExternalApiException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * This will log all external exception events (e.g. API calls) to a separate channel
+ * @package CampaignChain\CoreBundle\EventListener
+ */
+class ExternalExceptionListener
+{
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * ExternalExceptionListener constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+
+        $exception = $event->getException();
+
+        if ($exception instanceof ExternalApiException) {
+            $this->logger->error($exception->getMessage());
+        }
+    }
+
+}
