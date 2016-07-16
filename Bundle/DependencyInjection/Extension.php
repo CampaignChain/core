2640b8e065acb0fa3be51a454a7db7ecaf57469e
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:19 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- Bundle/DependencyInjection/Extension.php
+++ Bundle/DependencyInjection/Extension.php
@@ -15,34 +15,22 @@
  * limitations under the License.
  */
 
-namespace CampaignChain\CoreBundle\DependencyInjection;
+namespace CampaignChain\CoreBundle\Bundle\DependencyInjection;
 
 use CampaignChain\CoreBundle\Util\VariableUtil;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension as SymfonyExtension;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class Extension extends SymfonyExtension
+abstract class Extension extends SymfonyExtension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-
+    public function setParameters(ContainerBuilder $container, $config){
         $configStrings = VariableUtil::arrayConcatenate($config);
         foreach ($configStrings as $name => $node) {
             $container->setParameter($this->getAlias().'.'.$name, $node);
--- DependencyInjection/CampaignChainCoreExtension.php
+++ DependencyInjection/CampaignChainCoreExtension.php
@@ -17,6 +17,11 @@
 
 namespace CampaignChain\CoreBundle\DependencyInjection;
 
+use CampaignChain\CoreBundle\Bundle\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+
 /**
  * This is the class that loads and manages your bundle configuration
  *
@@ -24,4 +29,17 @@ namespace CampaignChain\CoreBundle\DependencyInjection;
  */
 class CampaignChainCoreExtension extends Extension
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $this->setParameters($container, $config);
+    }
 }
